55b2bb47bccba0504db2347f0f93a5f3ca8670e4
econejer <econejer@gmail.com>
2014-08-13 12:07:51 -0400

    final change of the JS interface

--- www/captureFG.js
+++ www/captureFG.js
@@ -1,94 +1,24 @@
-// cordova.define("com.tuxpan.foregroundvideocapture.captureFG", function(require, exports, module) {/*
-//  *
-//  * Licensed to the Apache Software Foundation (ASF) under one
-//  * or more contributor license agreements.  See the NOTICE file
-//  * distributed with this work for additional information
-//  * regarding copyright ownership.  The ASF licenses this file
-//  * to you under the Apache License, Version 2.0 (the
-//  * "License"); you may not use this file except in compliance
-//  * with the License.  You may obtain a copy of the License at
-//  *
-//  *   http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing,
-//  * software distributed under the License is distributed on an
-//  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-//  * KIND, either express or implied.  See the License for the
-//  * specific language governing permissions and limitations
-//  * under the License.
-//  *
-// */
+cordova.define("com.tuxpan.foregroundvideocapture.captureFG", function(require, exports, module) {
 
-// var exec = require('cordova/exec'), MediaFile = require('./MediaFile');
+    var captureFG = {
 
-// /**
-//  * Launches a capture of different types.
-//  *
-//  * @param (DOMString} type
-//  * @param {Function} successCB
-//  * @param {Function} errorCB
-//  * @param {CaptureVideoOptions} options
-//  */
-// function _captureFG(type, successCallback, errorCallback, options) {
-//     var win = function(pluginResult) {
-//         var mediaFiles = [];
-//         var i;
-//         for (i = 0; i < pluginResult.length; i++) {
-//             var mediaFile = new MediaFile();
-//             mediaFile.name = pluginResult[i].name;
-//             mediaFile.fullPath = pluginResult[i].fullPath;
-//             mediaFile.type = pluginResult[i].type;
-//             mediaFile.lastModifiedDate = pluginResult[i].lastModifiedDate;
-//             mediaFile.size = pluginResult[i].size;
-//             mediaFiles.push(mediaFile);
-//         }
-//         successCallback(mediaFiles);
-//     };
-//     exec(win, errorCallback, "CaptureFG", type, [options]);
-// }
-// /**
-//  * The Capture interface exposes an interface to the camera and microphone of the hosting device.
-//  */
-// function CaptureFG() {
-//     this.supportedVideoModes = [];
-// }
+        captureVideoFG : function(successCallback, errorCallback, options) {
+            var exec = require('cordova/exec');
 
-// /**
-//  * Launch device camera application for recording video(s).
-//  *
-//  * @param {Function} successCB
-//  * @param {Function} errorCB
-//  * @param {CaptureVideoOptions} options
-//  */
-// CaptureFG.prototype.captureVideoFG = function(successCallback, errorCallback, options){
-//     _captureFG("captureVideoFG", successCallback, errorCallback, options);
-// };
+            var win = function(pluginResult) {
 
+                var mediaFile = {};
+                mediaFile.name = pluginResult[0].name;
+                mediaFile.fullPath = pluginResult[0].fullPath;
+                mediaFile.type = pluginResult[0].type;
+                mediaFile.lastModifiedDate = pluginResult[0].lastModifiedDate;
+                mediaFile.size = pluginResult[0].size;
 
-// module.exports = new CaptureFG();
-// });
+                successCallback(mediaFile);
+            };
+            exec(win, errorCallback, "CaptureFG", "action", [ options ]);
+        }
 
-var captureFG = {
-    
-   captureVideoFG : function(successCallback, errorCallback, options){
-        var exec = require('cordova/exec');
-        var MediaFile = require('./MediaFile');
-        var win = function(pluginResult) {
-            var mediaFiles = [];
-            var i;
-            for (i = 0; i < pluginResult.length; i++) {
-                var mediaFile = new MediaFile();
-                mediaFile.name = pluginResult[i].name;
-                mediaFile.fullPath = pluginResult[i].fullPath;
-                mediaFile.type = pluginResult[i].type;
-                mediaFile.lastModifiedDate = pluginResult[i].lastModifiedDate;
-                mediaFile.size = pluginResult[i].size;
-                mediaFiles.push(mediaFile);
-            }
-        successCallback(mediaFiles);
-        };
-        exec(win, errorCallback, "CaptureFG", "action", [options]);
     }
-
-}
-module.exports = captureFG;
+    module.exports = captureFG;
+});
